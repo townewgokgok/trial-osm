518feb7b6f44aba98c8a40415dd6a2e701b04d64
k-furukawa <townewgokgok@gmail.com>
2023-10-04 16:53:04 +0900

    Use leaflet-extra-markers

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import L from "leaflet";
+import * as ExtraMarkers from 'leaflet-extra-markers';
+require('leaflet-extra-markers');
 import * as geojson from "geojson";
 import './styles/index.styl';
 
@@ -15,21 +17,23 @@ L.tileLayer(
   }
 ).addTo(mymap);
 
-const customIcon = L.divIcon({
-  html: `
-<svg
-  width="24"
-  height="40"
-  viewBox="0 0 100 100"
-  version="1.1"
-  preserveAspectRatio="none"
-  xmlns="http://www.w3.org/2000/svg"
->
-  <path d="M0 0 L50 100 L100 0 Z" fill="#7A8BE7"></path>
-</svg>`,
-  className: "",
-  iconSize: [24, 40],
-  iconAnchor: [12, 40],
+// const customIcon = L.divIcon({
+//   html: `<svg xmlns="http://www.w3.org/2000/svg" height="${iconSize}px" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M384 192c0 87.4-117 243-168.3 307.2c-12.3 15.3-35.1 15.3-47.4 0C117 435 0 279.4 0 192C0 86 86 0 192 0S384 86 384 192z"/></svg>`,
+//   className: "",
+//   iconSize: [iconSize * 0.75, iconSize],
+//   iconAnchor: [iconSize * 0.75 / 2, iconSize],
+// });
+
+const customIcon = L.ExtraMarkers.icon({
+  shape: 'circle',
+  markerColor: 'cyan',
+  prefix: 'fas',
+  icon: 'fa-dog',
+  iconColor: '#fff',
+  iconRotate: 0,
+  extraClasses: '',
+  number: '',
+  svg: true,
 });
 
 const data: geojson.FeatureCollection = {
@@ -62,14 +66,14 @@ L.geoJSON(data, {
 // イベント処理
 var popup = L.popup();
 mymap.on("click", e => {
-  popup.setLatLng(e.latlng).setContent(
-    "<p>緯度:" + e.latlng.lat + " 経度:" + e.latlng.lng + "</p>"
-  ).openOn(mymap);
+  // popup.setLatLng(e.latlng).setContent(
+  //   "<p>緯度:" + e.latlng.lat + " 経度:" + e.latlng.lng + "</p>"
+  // ).openOn(mymap);
 });
 
-L.marker([35.6927748, 139.6939962], {icon:customIcon}).addTo(mymap).on('click', e => {
+L.marker([35.6927524, 139.6950475], {icon:customIcon}).addTo(mymap).on('click', e => {
   popup
     .setLatLng(e.latlng)
-    .setContent("テキサス 野村ビル店" + e.latlng.toString())
+    .setContent("テキサス 野村ビル店")
     .openOn(mymap);
 });
